5df8c7696e6f6241dc342c7a777a446e5e0a8763
Ihor Utkin <igorutkin2002@gmail.com>
2024-09-22 21:39:28 +0200

    Resolve

---
+++ taxi/tests/test_forms.py
@@ -0,0 +1,112 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from taxi.forms import (
+    CarModelSearchForm,
+    ManufacturerNameSearchForm,
+    DriverLicenseUpdateForm,
+    DriverUsernameSearchForm,
+    DriverCreationForm
+)
+from taxi.models import Manufacturer
+
+
+class TestCarForms(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="new_driver",
+            password="root1234",
+        )
+        self.client.force_login(self.user)
+
+        self.manufacturer = Manufacturer.objects.create(
+            name="Test manufacture", country="USA"
+        )
+
+    def test_car_search_form_with_arg(self):
+        form_data = {
+            "model": "M5",
+        }
+        form = CarModelSearchForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_car_search_form_without_arg(self):
+        form_data = {
+            "model": "",
+        }
+        form = CarModelSearchForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+
+class TestManufacturerForms(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="new_driver",
+            password="root1234",
+        )
+        self.client.force_login(self.user)
+
+        self.manufacturer = Manufacturer.objects.create(
+            name="Test manufacture", country="USA"
+        )
+
+    def test_manufacturer_search_form_with_arg(self):
+        form_data = {
+            "name": "Test manufacture",
+        }
+        form = ManufacturerNameSearchForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_manufacturer_search_form_without_arg(self):
+        form_data = {
+            "name": "",
+        }
+        form = ManufacturerNameSearchForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+
+class TestDriverForms(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="new_driver",
+            password="root1234",
+        )
+        self.client.force_login(self.user)
+
+        self.manufacturer = Manufacturer.objects.create(
+            name="Test manufacture", country="USA"
+        )
+
+    def test_driver_form(self):
+        form_data = {
+            "username": "new_driver",
+            "license_number": "HRN84739",
+            "password1": "root1234!",
+            "password2": "root1234!"
+        }
+        form = DriverCreationForm(form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_driver_search_form_with_arg(self):
+        form_data = {
+            "username": "new_driver",
+        }
+        form = DriverUsernameSearchForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_driver_search_form_without_arg(self):
+        form_data = {
+            "username": "",
+        }
+        form = DriverUsernameSearchForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_update_license_form_correct_num(self):
+        form_data = {"license_number": "HRN84739"}
+        form = DriverLicenseUpdateForm(form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_update_license_form_incorrect_num(self):
+        form_data = {"license_number": "HrN8473"}
+        form = DriverLicenseUpdateForm(form_data)
+        self.assertEqual(form.is_valid(), False)
---
+++ taxi/tests/test_models.py
@@ -0,0 +1,50 @@
+from django.test import TestCase
+
+from taxi.models import Manufacturer, Car, Driver
+
+
+class ManufacturerModelTest(TestCase):
+    def test_manufacturer_output(self):
+        test_manufacture = Manufacturer.objects.create(
+            name="Test manufacture",
+            country="USA"
+        )
+
+        self.assertEqual(str(test_manufacture), f"{test_manufacture.name} {test_manufacture.country}")
+
+
+class DriverModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Driver.objects.create_user(
+            username="new_driver",
+            password="root1234",
+            first_name="Name",
+            last_name="Surname"
+        )
+
+    def test_driver_output(self):
+        driver = Driver.objects.get(id=1)
+        self.assertEqual(
+            str(driver),
+            f"{driver.username} ({driver.first_name} {driver.last_name})"
+        )
+
+    def test_get_root_address(self):
+        driver = Driver.objects.get(id=1)
+        self.assertEqual(driver.get_absolute_url(), "/drivers/1/")
+
+
+class CarModelTest(TestCase):
+    def test_car_output(self):
+        test_manufacture = Manufacturer.objects.create(
+            name="Test manufacture",
+            country="USA"
+        )
+
+        car = Car.objects.create(
+            model="M5",
+            manufacturer=test_manufacture,
+        )
+
+        self.assertEqual(str(car), car.model)
